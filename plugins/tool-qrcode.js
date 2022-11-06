b15da518d7e1eecc07e660a4713022c23fcb5846
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-11-06 20:29:10 +0800

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,7 +1,11 @@
 
 import { toDataURL } from 'qrcode'
 
-let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply("Masukkan textnya...!!")
+  conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+  
+}
 
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
