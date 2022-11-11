7e9d686134ec54fecc21632534d3a9bccc5a97ff
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-11-11 16:43:57 +0800

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.status == true) throw json
+    if (json.result == '') throw 'Maaf lirik lagu tidak ditemukan!'
     m.reply(`Lirik lagu dari ${text} adalah : \n\n${json.result}`)
 
 
